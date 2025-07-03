67581f01afae6b7495f5d6095ce4402605088d97
JulioCesar1012 <juliocsq13gmail@gmail.com>
2025-07-03 16:13:28 -0300

    Primeiro commit - Aula 01 de lógica

---
+++ main.py
@@ -0,0 +1,10 @@
+nome = input("Qual seu nome? ")
+curso = input("Qual curso você faz? ")
+idade = input("Quantos anos você tem? ")
+
+print()
+print("Olá,", nome + "!")
+print("Você está matriculado em", curso)
+print("E tem", idade, "anos")
+
+
